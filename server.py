4f34b4b905bb0d51242225b64dbf6aa644b3b8cf
Javier Rojas <jrojas03.08.98@gmail.com>
2020-07-21 22:53:15 -0400

    Added pymongo connection

--- server.py
+++ server.py
@@ -1,13 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from flask_pymongo import PyMongo
+from bson import json_util
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/tareaCompras"
+mongo = PyMongo(app)
+db = mongo.db
+
 
 @app.route("/")
 def index():
     return render_template('dashboard.html')
 
+
+@app.route('/movimientos', methods=['GET'])
+def get_movements():
+
+    movimientos = db.movimientoInventario.find()
+    response = json_util.dumps(movimientos)
+    print("response", response)
+
+    return Response(response, mimetype="application/json")
+
+
+@app.route('/movimientos', methods=['POST'])
+def insert_movement():
+
+    codigoMovimiento = request.json['codigoMovimiento']
+    codigoAlmacen = request.json['codigoAlmacen']
+    tipoMovimiento = request.json['tipoMovimiento']
+    codigoArticulo = request.json['codigoArticulo']
+    cantidad = request.json['cantidad']
+    unidad = request.json['unidad']
+
+
+db.movimientoInventario.insert_one(
+    {
+        "codigoMovimiento": codigoMovimiento,
+        "codigoAlmacen": codigoAlmacen,
+        "tipoMovimiento": tipoMovimiento.upper(),
+        "codigoArticulo": codigoArticulo,
+        "cantidad": cantidad,
+        "unidad": unidad,
+    },
+)
+return Response(response, mimetype="application/json")
+
+
 if __name__ == "__main__":
     app.run()
-
-
-
